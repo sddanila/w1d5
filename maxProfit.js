bb017e32709a6e9f436ca9c8fd33bfec36724a61
sddanila <sddanila@yahoo.co.uk>
2019-02-08 23:06:41 +0000

    add solution to the calculate maxProfit exercise

---
+++ maxProfit.js
@@ -0,0 +1,14 @@
+function maxProfit(arr){
+    var maxProfit = arr[1] - arr[0];
+    for(var i = 1; i < arr.length; i++){
+        for(var j = i - 1; j >= 0; j--){
+            if (maxProfit < (arr[i] - arr[i-j])){
+                maxProfit = arr[i] - arr[i-j];
+            }
+        }
+
+    }
+    return maxProfit;
+}
+
+console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
